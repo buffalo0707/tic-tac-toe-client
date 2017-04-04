429cdcd087039797f1e843a17259ed70b6b56374
buffalo0707 <alan.henderson@libertymutual.com>
2017-04-04 16:23:34 -0400

    Initial commit
    Starting from browser-template. This is the beginning of my project
    and will be a client that allows users to play a game of tic-tac-toe.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const store = require('../store.js')
+
+const signUpSuccess = (data) => {
+  console.log(data)
+}
+
+const signUpFailure = (error) => {
+  console.error(error)
+}
+
+const signInSuccess = (data) => {
+  console.log('success', data)
+  store.user = data.user
+}
+
+const signInFailure = (error) => {
+  console.log('failure', error)
+}
+
+const signOutSuccess = () => {
+  console.log('signOut success ran and nothing was returned')
+  store.user = {}
+}
+
+const signOutFailure = (error) => {
+  console.log('signOut failure ran. error is:', error)
+}
+
+const changePasswordSuccess = () => {
+  console.log('changePassword success ran and nothing was returned')
+}
+
+const changePasswordFailure = (error) => {
+  console.log('changePassword failure ran. error is:', error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}
